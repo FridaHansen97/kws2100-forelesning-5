03bb103dfe32cf2c2f34c05721776174a6108324
frida <frida1997@live.no>
2026-02-08 21:02:04 +0100

    style skoler based on props

--- src/components/layers/skoleLayer.tsx
+++ src/components/layers/skoleLayer.tsx
@@ -4,9 +4,31 @@ import { GeoJSON } from "ol/format.js";
 import { Circle, Fill, Stroke, Style, Text } from "ol/style.js";
 import type { FeatureLike } from "ol/Feature.js";
 
+interface SkoleProperties {
+  antall_ansatte: number;
+  antall_elever: number;
+  eierforhold: "Offentlig" | "Privat";
+  navn: string;
+}
+
+function skoleStyle(feature: FeatureLike) {
+  const props = feature.getProperties() as SkoleProperties;
+
+  return new Style({
+    image: new Circle({
+      radius: 4 + props.antall_elever / 100,
+      stroke: new Stroke({ width: 2, color: "white" }),
+      fill: new Fill({
+        color: props.eierforhold === "Offentlig" ? "blue" : "purple",
+      }),
+    }),
+  });
+}
+
 export const skoleLayer = new VectorLayer({
   source: new VectorSource({
     url: "public/skoler.geojson",
     format: new GeoJSON(),
   }),
+  style: skoleStyle,
 });
