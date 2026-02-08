4f33edb80b070ef971b46778d301377c284b381b
frida <frida1997@live.no>
2026-02-08 20:34:53 +0100

    style bydelsgrenser

--- src/components/app/application.tsx
+++ src/components/app/application.tsx
@@ -1,23 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Map, View } from "ol";
+import { type Feature, Map, MapBrowserEvent, View } from "ol";
 import TileLayer from "ol/layer/Tile.js";
 import { OSM, WMTS } from "ol/source.js";
 import { useGeographic } from "ol/proj.js";
 
-// Styling of OpenLayers components like zoom and pan controls.
-// By default, TypeScript doesn't know how to validate CSS files, so we
-// add the next line to avoid compilation errors (for a better solution,
-// see the reference code for lecture 3)
 // @ts-ignore
 import "ol/ol.css";
 import { Layer } from "ol/layer.js";
-import VectorLayer from "ol/layer/Vector.js";
-import { GeoJSON, WMTSCapabilities } from "ol/format.js";
 import { optionsFromCapabilities } from "ol/source/WMTS.js";
-import VectorSource from "ol/source/Vector.js";
+import { WMTSCapabilities } from "ol/format.js";
+import { bydelLayer } from "../layers/bydelLayer.js";
+import VectorLayer from "ol/layer/Vector.js";
 
-// By calling the "useGeographic" function in OpenLayers, we tell that we want coordinates to be in degrees
-//  instead of meters, which is the default. Without this `center: [10.6, 59.9]` brings us to "null island"
 useGeographic();
 
 const view = new View({ center: [10.9, 59.9], zoom: 12 });
@@ -40,12 +34,6 @@ fetch("https://cache.kartverket.no/v1/wmts/1.0.0/WMTSCapabilities.xml").then(
   },
 );
 
-const bydelLayer = new VectorLayer({
-  source: new VectorSource({
-    url: "/public/bydeler.geojson",
-    format: new GeoJSON(),
-  }),
-});
 const skoleLayer = new VectorLayer();
 
 export function Application() {
